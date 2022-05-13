dbdec72f1e8d4dab585d5d50ed8d282735e13621
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2022-05-13 18:31:21 +0100

    add responsive css and logo to navbar

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-fetch('http://localhost:1337/api/tourdate')
-.then(response => response.json())
-.then(data => console.log(data));
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+
+//Loigc to toggle the "hamburger" menu in the mobile view
+const mobileMenuToggle = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('mobile-view');
+    }
+
+
+    menu.addEventListener('click', mobileMenuToggle);
