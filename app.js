58f77b8fb22ce7372d8f29b86eea7ac7026d50e7
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2022-05-17 18:03:25 +0100

    fix navbar overlap on mobile

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navbar = document.querySelector('.navbar');
 
 
 //Loigc to toggle the "hamburger" menu in the mobile view
 const mobileMenuToggle = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('mobile-view');
+    navbar.classList.toggle('navbar-mobile');
     }
 
 
