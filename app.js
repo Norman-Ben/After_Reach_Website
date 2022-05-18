b025aa8d0410274237b0d932b8d6c3a5a5e5bc9f
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2022-05-18 18:13:10 +0100

    add carousel and improve accessibility

--- app.js
+++ app.js
@@ -2,13 +2,36 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navbar = document.querySelector('.navbar');
 
-
 //Loigc to toggle the "hamburger" menu in the mobile view
 const mobileMenuToggle = () => {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('mobile-view');
-    navbar.classList.toggle('navbar-mobile');
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('mobile-view');
+  navbar.classList.toggle('navbar-mobile');
+};
+
+menu.addEventListener('click', mobileMenuToggle);
+
+//Logic for the image carousel
+
+const buttons = document.querySelectorAll('[data-carousel-button]');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+    const slides = button
+      .closest('[data-carousel]')
+      .querySelector('[data-slides]');
+
+    const activeSlide = slides.querySelector('[data-active]');
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) {
+      newIndex = slides.children.length - 1;
+    }
+    if (newIndex >= slides.children.length) {
+      newIndex = 0;
     }
 
-
-    menu.addEventListener('click', mobileMenuToggle);
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
