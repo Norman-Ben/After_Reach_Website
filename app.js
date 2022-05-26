87b0ae8f321c642c0f0464c6db59d9155168871f
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2022-05-26 14:13:05 +0100

    remove old css carousel and add swiperJS carousel

--- app.js
+++ app.js
@@ -11,39 +11,12 @@ const mobileMenuToggle = () => {
 
 menu.addEventListener('click', mobileMenuToggle);
 
-//Logic for the image carousel
-
-const buttons = document.querySelectorAll('[data-carousel-button]');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
-    const slides = button
-      .closest('[data-carousel]')
-      .querySelector('[data-slides]');
-
-    const activeSlide = slides.querySelector('[data-active]');
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) {
-      newIndex = slides.children.length - 1;
-    }
-    if (newIndex >= slides.children.length) {
-      newIndex = 0;
-    }
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
-  });
-});
-
-
-
 //Logic to call Strapi API to fetch the tourdate data and populate the tourdates list
 
 fetch('http://localhost:1337/api/tourdate')
-.then(response => response.json())
-.then(data => {
-  document.querySelector('.tourdates').innerHTML = data.data.attributes.ListOfShows
-})
-.catch(error => console.log(error));
-
+  .then((response) => response.json())
+  .then((data) => {
+    document.querySelector('.tourdates').innerHTML =
+      data.data.attributes.ListOfShows;
+  })
+  .catch((error) => console.log(error));
